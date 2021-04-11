db8e88505ff4204852d14658cefc7d957c206e00
Marek Mikula <marek.mikula01@gmail.com>
2021-04-11 13:32:37 +0200

    Much better storyline

--- src/en/mikula/adventure/items/terminals/BridgeTerminal.java
+++ src/en/mikula/adventure/items/terminals/BridgeTerminal.java
@@ -43,9 +43,10 @@ public class BridgeTerminal extends Terminal {
             case 0: // last diary entry
                 System.out.println("18.02.2159 01:16 - The miners should be there in like an hour. There were supposed to be some ill miners as the shift leader stated that something jumped on their heads while working in the mine. At least they got it off their heads and they are on their way to Nostromo. But I am really worried about them because they were not responding for the last couple hours of the flight...");
                 System.out.println("18.02.2159 03:16 - The ship just arrived, no one is responding. We'll go to check the ship out in the cargo space room. Ryan");
+                System.out.println("18.02.2159 04:26 - We managed to open the ship. There were some terrible creatures inside and all the miners were dead. Autopilot navigated the ship safe to Nostromo not knowing what was inside. We locked them in the cargo space room and we will try to use escape modules quikcly. Our leading commander didn't make it. We left him in the cafeteria. As soon as we get into the escape module sector, we will block the entrance. May the God be with us.");
                 break;
             case 1: // status of escape modules
-                System.out.println("Number of escape modules used: 36");
+                System.out.println("Number of escape modules used: 76");
                 System.out.println("Number of escape modules left: 1");
                 break;
             case 2: // close terminal
